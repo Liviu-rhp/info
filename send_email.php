0c4ff432e29129cbf0ebe4cea683bef8e11aa5bb
lhmetalart <hrisculiviuandrei@gmail.com>
2025-01-14 07:57:28 -0800

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nume = htmlspecialchars($_POST['nume']);
+    $email = htmlspecialchars($_POST['email']);
+    $mesaj = htmlspecialchars($_POST['mesaj']);
+
+    // Adresa ta de e-mail
+    $to = "adresa-ta@example.com";
+    $subject = "Mesaj de la $nume";
+    $body = "Nume: $nume\nEmail: $email\n\nMesaj:\n$mesaj";
+    $headers = "From: $email";
+
+    // Trimitere e-mail
+    if (mail($to, $subject, $body, $headers)) {
+        echo "<script>alert('Mesajul a fost trimis cu succes!'); window.location.href='contact.html';</script>";
+    } else {
+        echo "<script>alert('Mesajul nu a fost trimis. Vă rugăm să ne contactați telefonic.'); window.location.href='contact.html';</script>";
+    }
+}
+?>
